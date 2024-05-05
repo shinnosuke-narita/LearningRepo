2466a2f649712e4f3d383f7ab4b24da641de4f1e
sin0713 <narisin1124@gmail.com>
2024-05-05 09:57:42 +0900

    [Add] カスタムテーマ適用

--- KMPSample/composeApp/src/androidMain/kotlin/com/example/project/MainActivity.kt
+++ KMPSample/composeApp/src/androidMain/kotlin/com/example/project/MainActivity.kt
@@ -5,12 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.project.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContent { MainScreen() }
+        setContent {
+            AppTheme { MainScreen() }
+        }
     }
 }
 
--- KMPSample/composeApp/src/commonMain/kotlin/com/example/project/MainScreen.kt
+++ KMPSample/composeApp/src/commonMain/kotlin/com/example/project/MainScreen.kt
@@ -1,13 +1,10 @@
 package com.example.project
 
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.navigator.Navigator
 import com.example.project.ui.index.IndexScreen
 
 @Composable
 internal fun MainScreen() {
-    MaterialTheme {
-        Navigator(IndexScreen())
-    }
+    Navigator(IndexScreen())
 }
